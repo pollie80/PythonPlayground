10b80306eb52d0d7f1e3e00aa6b30fa6875d6c9d
Tian Welgemoed <tian.welgemoed@gmail.com>
2023-11-08 00:16:28 +1300

    using 3d array and working out coordinates

--- Valorant/instalocker.py
+++ Valorant/instalocker.py
@@ -1,33 +1,51 @@
 import pyautogui
 import keyboard
+import sys
 
-# todo use listener
-# todo use 3d array instead
-selected_agent = "sova"
-# where the agents are on screen
-agents_positions = {
-    "astra": (625, 840),
-    "breach": (715, 840),
-    "brim": (790, 840),
-    "chamber": (790, 840),
-    "cypher": (790, 840),
-    "deadlock": (790, 840),
-    "fade": (790, 840),
-    "gekko": (790, 840),
-    "harbour": (790, 840),
-    "iso": (625, 915),
-    "raze": (1215, 915)
-}
-lock_positions = (955, 725)
+agent_block_size = 85
+lock_coordinate = (955, 725)
+first_agent_coordinate = (615, 845)
+selected_agent = "Yoru"
+# agents orders
+agents_positions = [
+    ["Astra", "Breach", "Brimstone", "Chamber", "Cypher", "Deadlock", "Fade", "Gekko", "Harbour"],
+    ["Iso", "Jett", "KAY/O", "Killjoy", "Neon", "Omen", "Phoenix", "Raze", "Reyna"],
+    ["Sage", "Skye", "Sova", "Viper", "Yoru"]
+]
+agent_row = -1
+agent_col = -1
+
+for row_idx, row in enumerate(agents_positions):
+    if selected_agent in row:
+        agent_row = row_idx
+        agent_col = row.index(selected_agent)
+        break
+# print(f"r,c {agent_row, agent_col}") # test correct position
+if agent_row != -1 and agent_col != -1:
+    selected_agent_coordinate = (
+        first_agent_coordinate[0] + agent_col * agent_block_size,
+        first_agent_coordinate[1] + agent_row * agent_block_size
+    )
+    print(f"Coordinates for {selected_agent}: {selected_agent_coordinate}")
+else:
+    print(f"{selected_agent} not found in the agents_positions list.")
+    sys.exit()
+
+
+def on_f12_pressed(e):
+    if e.event_type == keyboard.KEY_DOWN:
+        pyautogui.moveTo(selected_agent_coordinate)
+        pyautogui.click()
+        pyautogui.moveTo(lock_coordinate)
+        pyautogui.click()
+
+
+keyboard.on_press_key("F12", on_f12_pressed)
+keyboard.wait("esc")
 
 # Perform the mouse movement and clicking while F12 is held down
-while True:
-    if keyboard.is_pressed('F12'):
-        pyautogui.moveTo(agents_positions[selected_agent])
-        pyautogui.click()
-        pyautogui.moveTo(lock_positions)
-        pyautogui.click()
-    # to get position
+# while True:
+    # # to get position
     # event = pyautogui.mouseInfo()
     # if event["left"]:
     #     x, y = pyautogui.position()
